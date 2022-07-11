280c846949dd8cb0ee2450ede138ebdeafa6ed92
Harry.Song <64049788+Harry-SJH@users.noreply.github.com>
2022-07-11 14:32:24 +0800

    Add files via upload

---
+++ js/road_g4_line1.js
@@ -0,0 +1,20 @@
+// 折线的节点坐标数组，每个元素为 AMap.LngLat 对象
+var path_1 = 
+[
+	new AMap.LngLat(114.91002,38.44186),
+	new AMap.LngLat(114.870421,38.44474),
+	new AMap.LngLat(114.86422,38.43704),
+	new AMap.LngLat(114.854316,38.426261),
+	new AMap.LngLat(114.854416,38.426261),
+	new AMap.LngLat(114.842987,38.411251),
+	new AMap.LngLat(114.83331,38.40582),
+	new AMap.LngLat(114.80951,38.3918),
+	new AMap.LngLat(114.796317,38.384995),
+];
+
+var path_2 = 
+[
+	new AMap.LngLat(115.79683,38.3851),
+	new AMap.LngLat(115.870421,38.44474),
+
+];
